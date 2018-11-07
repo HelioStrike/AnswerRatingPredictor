ef4a89346bdaa9b1064897c2a73a92f5895a0951
HelioStrike <rageofepicfury@gmail.com>
2018-11-07 13:47:06 +0530

    committed

--- Vocab.py
+++ Vocab.py
@@ -1,9 +1,12 @@
 import string
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
 
 def removeSpecial(text):
     return "".join([c for c in text if c not in string.punctuation])
 
 def cleanText(text):
+    text = " ".join([w for w in text.split("-")])
     text = removeSpecial(text).lower()
     return " ".join([w for w in text.split(" ") if w is not ""])
 
@@ -15,10 +18,15 @@ def getVocab(fname):
 
 class Vocabulary:
     def __init__(self, text):
+        self.lmt = WordNetLemmatizer()
+
         self.vocab = []
         for w in text.split(' '):
-            if w not in self.vocab:
-                self.vocab.append(w)
+            word = self.lmt.lemmatize(w, 'v')
+            if word == w:
+                word = self.lmt.lemmatize(w, 'n')
+            if word not in self.vocab:
+                self.vocab.append(word)
         self.vocab.append("<dne>")
 
         self.vocab_size = len(self.vocab)
@@ -26,7 +34,17 @@ class Vocabulary:
         self.ix2word = {self.vocab[i]:i for i in range(len(self.vocab))}
             
     def getSentenceArray(self, text):
-        return [self.word2ix[w] if w in self.vocab else self.word2ix["<dne>"] for w in text.split(' ')]
+        text = cleanText(text)
+        arr = []
+        for w in text.split(' '):
+            word = self.lmt.lemmatize(w, 'v')
+            if word == w:
+                word = self.lmt.lemmatize(w, 'n')
+            if word in self.vocab:
+                arr.append(self.word2ix[word])
+            else:
+                arr.append(self.word2ix["<dne>"])
+        return arr
     
     def size(self):
         return self.vocab_size
