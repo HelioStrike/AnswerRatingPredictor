d629dd8f3d6906a3d72ff4e11c7388da4c0cc097
HelioStrike <rageofepicfury@gmail.com>
2019-01-31 20:08:55 +0530

    committed

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 #attention model
 class AttentionModel(nn.Module):
@@ -28,3 +29,38 @@ class AttentionModel(nn.Module):
         score = F.sigmoid(self.l2(score))
         
         return score
+
+def vectorMod(vec):
+    return (torch.sum(vec**2))**(0.5)
+
+#attention + cosine similarity
+class AttentionModel2(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size):
+        super(AttentionModel2, self).__init__()
+        self.hidden_size = hidden_size
+        
+        self.embeds = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size)
+        self.l1 = nn.Linear(hidden_size, 100)
+        self.lin_attn = nn.Linear(hidden_size, 1)
+    
+
+    def forward(self, corr, test):
+        corr = self.embeds(corr)
+        test = self.embeds(test)
+        _, (corr, _) = self.lstm(corr.unsqueeze(0))
+        _, (test, _) = self.lstm(test.unsqueeze(0))
+        corr_attn_params = F.softmax(self.lin_attn(corr).view(1, -1))
+        test_attn_params = F.softmax(self.lin_attn(test).view(1, -1))
+        corr_attn = torch.matmul(corr_attn_params, corr.squeeze(0))
+        test_attn = torch.matmul(test_attn_params, test.squeeze(0))
+        corr_attn = torch.l1(corr_attn)
+        test_attn = torch.l1(test_attn)
+
+        score = torch.sum(corr_attn*test_attn)/(vectorMod(corr_attn)*vectorMod(test_attn))
+
+        return (score+1)/2
+
+def cosineSimilarity(vec1, vec2):
+    assert len(vec1) == len(vec2)
+    return np.dot(vec1, vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, abort, redirect, url_for, request
 import torch
 import random
 import pandas as pd
+import os
 
 from Vocab import *
 from model import AttentionModel
@@ -10,11 +11,20 @@ app = Flask(__name__)
 model = None
 vocab = None
 questions = None
-model_path = "saved_models/stsds9.pt"
+
+#Paths and Hyperparams
+model_path = "saved_models/stsds6.pt"
 vocab_path = "stsds-cat.txt"
 embed_size = 128
 hidden_size = 256
 
+#Variables to store make_data's data
+ans_set = None
+ids = []
+curr_questions = []
+tampered_answers = []
+
+#Returns answer rating as a float
 def return_rating(corr, res):
     corr_sentence = cleanText(corr)
     test_sentence = cleanText(res)
@@ -24,6 +34,7 @@ def return_rating(corr, res):
 
     return rating
 
+#Returns answer grade
 def return_grade(corr, res):
     rating = return_rating(corr, res)
 
@@ -40,19 +51,25 @@ def return_grade(corr, res):
 
     return rating, grade
 
+#Returns answer(sentence) grade given qid
 @app.route('/rate/<qid>/<sentence>')
+@app.route('/rate/<qid>/<sentence>/')
 def get_grade(qid, sentence):
     answer = " ".join([w for w in sentence.split('+')])
     _, grade = return_grade(correct_answers[qid], answer)
     
     return questions[int(qid)]+" : "+grade
 
+#Compares corr and res, and returns grade 
 @app.route('/compare/<corr>/<res>')
 def get_grade_sentences(corr, res):    
     return return_grade(" ".join([w for w in corr.split('+')]), " ".join([w for w in res.split('+')]))[1]
 
+#Model testing module
 @app.route('/test')
+@app.route('/test/')
 @app.route('/test/<qid>', methods=['GET', 'POST'])
+@app.route('/test/<qid>/', methods=['GET', 'POST'])
 def test(qid=None):
     if qid is None:
         qid = random.randint(0, len(questions)-1)
@@ -65,12 +82,40 @@ def test(qid=None):
             return render_template("test.html", qid=qid, question=question, answer=answer, grade=grade, rating=rating)
 
         return render_template("test.html", qid=qid, question=question)
-            
 
+#Module to make data
+@app.route('/make_data', methods=['GET', 'POST'])
+@app.route('/make_data/', methods=['GET', 'POST'])
+def make_data():
+    global ids
+    global tampered_answers
+    if request.method == 'POST':
+        for i in range(3):
+            try:
+                curr_rating = max(0,min(float(request.form["grade"+str(i)]),1)) #clamps the value between 0 and 1
+                ans_set.loc[len(ans_set)] = [correct_answers[ids[i]], tampered_answers[i], curr_rating]
+            except:
+                pass
+        ans_set.to_csv("ansset.csv", index=False)
+        return redirect('/make_data')
+    else:
+        ids = [random.randint(0,len(questions)-1) for i in range(3)]
+        curr_questions = [questions[id] for id in ids]
+        tampered_answers = [tamper_text(correct_answers[id]) for id in ids]
+        return render_template("make_data.html", questions=curr_questions, answers=tampered_answers)
+
+#Start setup
 if __name__=="__main__":
     data = pd.read_csv("qna.csv")
     questions = data["question"]
     correct_answers = data["answer"]
     vocab = getVocab(vocab_path)
     model = torch.load(model_path)
+
+    if "ansset.csv" not in os.listdir('.'):
+        ans_set = pd.DataFrame(columns=["correct_answer","response_answer","grade"])
+        ans_set.to_csv("ansset.csv", index=False)
+    else:
+        ans_set = pd.read_csv("ansset.csv")
+
     app.run(host="0.0.0.0", port="5010")
